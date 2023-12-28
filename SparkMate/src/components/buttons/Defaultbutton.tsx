4f91c171479f550fbb5de8aa2c9a477bbdbf35fb
TubiOb <Tubiobaloluwa@gmail.com>
2023-12-28 20:31:29 +0100

    added components for login page

--- SparkMate/src/components/buttons/Defaultbutton.tsx
+++ SparkMate/src/components/buttons/Defaultbutton.tsx
@@ -2,11 +2,11 @@ import { Box, Button as ChakraButton, ButtonProps as CBProps } from "@chakra-ui/
 import { DefaultbuttonProps } from "../../interface"
 
 const Defaultbutton = ( props: DefaultbuttonProps ) => {
-    const { width, bg, color, shadow, children, type='button', onClick, isLoading, loadingText, borderWidth, borderColor, fontSize } = props;
+    const { width, bg, color, shadow, children, className, type='button', onClick, isLoading, loadingText, borderWidth, borderColor, fontSize } = props;
 
   return (
     <Box>
-        <ChakraButton _hover={{opacity: 0.7,}} bg={bg} color={color} h={['40px', '45px', '50px']} shadow={shadow} width={width} onClick={onClick} type={type} isLoading={isLoading} loadingText={loadingText} borderWidth={borderWidth} borderColor={borderColor} fontSize={fontSize}>
+        <ChakraButton _hover={{opacity: 0.7,}} bg={bg} color={color} h={['40px', '45px', '50px']} shadow={shadow} fontFamily='Manrope' className={className} width={width} onClick={onClick} type={type} isLoading={isLoading} loadingText={loadingText} borderWidth={borderWidth} borderColor={borderColor} fontSize={fontSize}>
             {children}
         </ChakraButton>
     </Box>
